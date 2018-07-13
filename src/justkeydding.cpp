6721ece8b66808e8201886c9b3bd549b522fdb52
Nestor Napoles <napulen@gmail.com>
2018-07-13 16:26:37 -0400

    Style: Using cpplint to improve the aesthetics of the code

--- src/justkeydding.cpp
+++ src/justkeydding.cpp
@@ -1,4 +1,4 @@
-/* June 2018, Nestor Napoles 
+/* Copyright 2018 Nestor Napoles 
 For now, the code is only reading a csv with the features already
 extracted (e.g., using nnls-chroma or essentia). This should be
 substituted in the future for the chromagram extraction done here.
@@ -27,8 +27,8 @@ struct entry{
     std::array<int, 12> magnitudes;
 };
 
-int getMagnitude(float magnitude){
-    return (int)magnitude;
+int getMagnitude(float magnitude) {
+    return static_cast<int>(magnitude);
 }
 
 /* For now, this function reads a csv with computed chromagram
@@ -36,49 +36,49 @@ int getMagnitude(float magnitude){
    tion symbols for the HMM */
 int main(int argc, char *argv[]) {
     // Parsing args
-    if(argc != 2) {
-        std::cout << "Missing an argument" 
-                  << std::endl << argv[0] 
+    if (argc != 2) {
+        std::cout << "Missing an argument"
+                  << std::endl << argv[0]
                   << "<input_file>" << std::endl;
-        // TODO: Add a proper argparse and help
+        // TODO(napulen): Add a proper argparse and help
         return RTRN_MISSING_ARG;
     }
     // Reading the csv
     std::ifstream infile;
-    infile.open(argv[1]);    
-    if(!infile.is_open()) {
+    infile.open(argv[1]);
+    if (!infile.is_open()) {
         std::cout << "Couldn't open your file." << std::endl;
         return RTRN_FILE_ERROR;
     }
     std::string line;
     std::vector<entry> rows;
-    while(std::getline(infile, line)) {
+    while ( std::getline(infile, line) ) {
         std::istringstream sstr(line);
         std::string token;
         int i = 0;
         struct entry x;
-        while(std::getline(sstr, token, ',')) {            
-            if(!i){
-                x.time = std::stof(token);          
-            } else{
+        while ( std::getline(sstr, token, ',') ) {
+            if (!i) {
+                x.time = std::stof(token);
+            } else {
                 x.magnitudes[i-1] = getMagnitude(std::stof(token));
             }
             i++;
         }
-        if(rows.empty()){
+        if (rows.empty()) {
             rows.push_back(x);
-        } else if(x.magnitudes != rows.back().magnitudes){
+        } else if (x.magnitudes != rows.back().magnitudes) {
             rows.push_back(x);
         }
     }
-    for(std::vector<entry>::const_iterator it = rows.begin(); 
-        it != rows.end(); it++) {
-        std::cout << it->time << ":" 
-                  << std::endl << "\t";     
-        for(int pc=0; pc < 12; pc++){
+    for (std::vector<entry>::const_iterator it = rows.begin();
+            it != rows.end(); it++) {
+        std::cout << it->time << ":"
+                  << std::endl << "\t";
+        for (int pc=0; pc < 12; pc++) {
             int pcIndex = (3+pc) % PITCH_CLASSES;
             int pcMagnitude = it->magnitudes[pcIndex];
-            std::cout << (int)pcMagnitude << " ";
+            std::cout << static_cast<int>(pcMagnitude) << " ";
         }
         std::cout << std::endl;
     }
