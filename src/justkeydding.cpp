c496c5470643e6ebd88061f41f4713257859582f
Nestor Napoles <napulen@gmail.com>
2018-07-13 23:22:00 -0400

    New: Adding a preprocessingChromagrams function

--- src/justkeydding.cpp
+++ src/justkeydding.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2018 Nestor Napoles 
+/* Copyright 2018 Néstor Nápoles 
 For now, the code is only reading a csv with the features already
 extracted (e.g., using nnls-chroma or essentia). This should be
 substituted in the future for the chromagram extraction done here.
@@ -25,14 +25,18 @@ enum status {
 
 #define PITCH_CLASSES 12
 
-char *programName;
+typedef std::array<float, PITCH_CLASSES> pitchClassArray;
 
 struct chromagram {
     float time;
-    std::array<int, 12> magnitudes;
+    pitchClassArray magnitudes;
 };
 
-int getChromagrams(char *audioFile, std::vector<chromagram> *chromagrams) {
+typedef std::vector<chromagram> chromagramVector;
+
+char *programName;
+
+int getChromagrams(char *audioFile, chromagramVector *chromagrams) {
     std::ifstream infile;
     infile.open(audioFile);
     if (!infile.is_open()) {
@@ -58,6 +62,26 @@ int getChromagrams(char *audioFile, std::vector<chromagram> *chromagrams) {
     return STATUS_OK;
 }
 
+int preprocessChromagrams(chromagramVector *chromagrams) {
+    for (chromagramVector::const_iterator it = chromagrams->begin();
+         it != chromagrams->end();
+         it++) {
+        if (it->magnitudes == (it+1)->magnitudes) {
+            // Remove duplicates
+            continue;
+        }
+        for (int pc=0; pc < 12; pc++) {
+            // The original order of the chromagram
+            // pitch-classes is A-G#, we want it to be C-B
+            int pcIndex = (3+pc) % PITCH_CLASSES;
+            float pcMagnitude = it->magnitudes[pcIndex];
+            std::cout << pcMagnitude << " ";
+        }
+        std::cout << std::endl;
+    }
+    return STATUS_OK;
+}
+
 int getMagnitude(float magnitude) {
     // This function determines how to "discretize"
     // the magnitude of a pitch-class in the chromagram
@@ -76,39 +100,30 @@ void logError(int status) {
         default:
         msg = "There was an error running the program.";
     }
-    msg += "\n\nUsage:\n" + std::string(programName) + " <input_file>\n";
+    msg += "\n\nUsage:\n" + std::string(programName) + " <input_file>";
     std::cerr << "Error " << status << ": " << msg << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-    // Parsing args
     int status;
     // This should always exist, right?
     programName = argv[0];
     if (argc != 2) {
-        // TODO(napulen): Add a proper argparse and help
+        // TODO(napulen): Add a proper argparse and help/usage
         status = STATUS_MISSING_ARG;
         logError(status);
         return status;
     }
-    std::vector<chromagram> chromagrams;
+    chromagramVector chromagrams;
+    // Get the chromagrams
     if ((status = getChromagrams(argv[1], &chromagrams)) != STATUS_OK) {
         logError(status);
         return status;
     }
-    for (std::vector<chromagram>::const_iterator it = chromagrams.begin();
-         it != chromagrams.end();
-         it++) {
-        std::cout << it->time << ":"
-                  << std::endl << "\t";
-        for (int pc=0; pc < 12; pc++) {
-            // The original order of the chromagram
-            // pitch-classes is A-G#, we want it to be C-B
-            int pcIndex = (3+pc) % PITCH_CLASSES;
-            int pcMagnitude = it->magnitudes[pcIndex];
-            std::cout << static_cast<int>(pcMagnitude) << " ";
-        }
-        std::cout << std::endl;
+    // Any pre-processing before "discretizing"
+    if ((status = preprocessChromagrams(&chromagrams)) != STATUS_OK) {
+        logError(status);
+        return status;
     }
-    return 0;
+    return STATUS_OK;
 }
