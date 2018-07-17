08853b073162309b5d6ef728d49909e4efe33ebb
Nestor Napoles <napulen@gmail.com>
2018-07-17 16:43:42 -0400

    Doc: Add a simple test program for reading key profiles

---
+++ test/test_key_profiles.cpp
@@ -0,0 +1,39 @@
+/*
+MIT License
+
+Copyright (c) 2018 Nestor Napoles
+
+Common key-profiles for key detection algorithms
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR sOTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include<map>
+#include<array>
+#include<iostream>
+
+#include "key_profiles.hpp"
+
+int main(int argc, char *argv[]) {
+    const hmm::key_profile kp =
+        hmm::normalized_key_profiles.sapp;
+    for (int i = 0; i < 12; i++) {
+        std::cout << kp.major[i] << std::endl;
+    }
+}
