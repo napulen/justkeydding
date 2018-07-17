eb66f50c914ea9904e1cc78faad7fa1eade6b548
Nestor Napoles <napulen@gmail.com>
2018-07-17 14:53:16 -0400

    New: Adding license to the header of the main C++ source

--- src/justkeydding.cpp
+++ src/justkeydding.cpp
@@ -1,14 +1,28 @@
-/* Copyright 2018 Néstor Nápoles 
-For now, the code is only reading a csv with the features already
-extracted (e.g., using nnls-chroma or essentia). This should be
-substituted in the future for the chromagram extraction done here.
+/*
+MIT License
 
-Some things to do:
-  * Embed chromagram computation from nnls
-  * Store the output in some data structure
-  * Implement a viterbi algorithm to find the best path
-  * Import the weights I computed in the symbolic version
-  * Generate a cool dataset to test this before submission */
+Copyright (c) 2018 Nestor Napoles
+
+Audio key detection program based on chromagrams and a hidden markov model (HMM)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include<iostream>
 #include<fstream>
