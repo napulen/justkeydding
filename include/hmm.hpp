164ec1df913e2679d0590b88ac0579da2670db24
Nestor Napoles <napulen@gmail.com>
2018-07-18 17:07:51 -0400

    New: Adding a template header for the hmm

---
+++ include/hmm.hpp
@@ -0,0 +1,37 @@
+/*
+MIT License
+
+Copyright (c) 2018 Nestor Napoles
+
+Hidden Markov Model for Key Detection
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR sOTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef INCLUDE_HMM_HPP_
+#define INCLUDE_HMM_HPP_
+
+#include<string>
+#include<map>
+
+namespace hmm {
+
+}  // namespace hmm
+
+#endif  // INCLUDE_HMM_HPP_
