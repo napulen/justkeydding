804640a1dc398348a4c42b24b28058b330e1c8f8
Nestor Napoles <napulen@gmail.com>
2018-12-24 15:32:08 -0500

    New: Correcting non_diatonics of the minor key profile generator

--- optimizer/generator.py
+++ optimizer/generator.py
@@ -1,17 +1,24 @@
 import random
 import logging
 
+
 class Generator:
     def __init__(self, maximum_range):
+        self.logger = logging.getLogger('generator')
         self.maximum_range = maximum_range
+        self.logger.info("Generator() <- maximum_range={}".format(self.maximum_range))
 
     def generate_key_profile(self):
         ''' Generate a set of random key profiles '''
+        self.logger.info('Start generate_key_profile()')
         major = self.generate_major_key_profile()
         minor = self.generate_minor_key_profile()
-        return major + minor
+        key_profile = major + minor
+        self.logger.info('Done generate_key_profile() -> key_profile={}'.format(key_profile))
+        return key_profile
 
     def generate_major_key_profile(self):
+        self.logger.info('Start generate_major_key_profile()')
         key_profile = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
         ]
@@ -27,17 +34,21 @@ class Generator:
             for idx in group:
                 r = random.randrange(int(self.maximum_range / 2**hierarchy))
                 key_profile[idx] = r
+                self.logger.debug('{}: {}'.format(idx, r))
         s = sum(key_profile)
-        return [x/s for x in key_profile]
+        key_profile = [x/s for x in key_profile]
+        self.logger.info('Done generate_major_key_profile() -> key_profile={}'.format(key_profile))
+        return key_profile
 
     def generate_minor_key_profile(self):
+        self.logger.info('Start generate_minor_key_profile()')
         key_profile = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
         ]
         tonic_dominant = [0, 7]
         harmonic = [2, 3, 5, 8, 11]
-        natural_melodic = [4, 9, 10]
-        non_diatonic = [1, 6]
+        natural_melodic = [9, 10]
+        non_diatonic = [1, 4, 6]
         groups = [
             (tonic_dominant, 0),
             (harmonic, 1),
@@ -48,9 +59,15 @@ class Generator:
             for idx in group:
                 r = random.randrange(int(self.maximum_range / 2**hierarchy))
                 key_profile[idx] = r
+                self.logger.debug('{}: {}'.format(idx, r))
         s = sum(key_profile)
-        return [x/s for x in key_profile]
+        key_profile = [x/s for x in key_profile]
+        self.logger.info('Done generate_minor_key_profile() -> key_profile={}'.format(key_profile))
+        return key_profile
 
     def generate_population(self, count):
         ''' Generate <count> key_profiles for a population '''
-        return [self.generate_key_profile() for _ in range(count)]
+        self.logger.info('Start generate_population() <- count={}'.format(count))
+        population = [self.generate_key_profile() for _ in range(count)]
+        self.logger.info('Done generate_population() -> population={}'.format(population))
+        return population
