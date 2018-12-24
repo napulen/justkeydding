27c212ac40e256652ae3c5ce801e9218a4bc5216
Nestor Napoles <napulen@gmail.com>
2018-12-24 15:30:59 -0500

    New: Multi-threaded evaluation

--- optimizer/evaluator.py
+++ optimizer/evaluator.py
@@ -1,27 +1,36 @@
 import subprocess
 import logging
+from multiprocessing.dummy import Pool as ThreadPool
 
 class Evaluator:
     def __init__(self, dataset):
         with open(dataset) as f:
+            self.logger = logging.getLogger('evaluator')
+            self.logger.info('Evaluator() <- dataset={}'.format(dataset))
             self.files = f.readlines()
             self.files = [x.strip() for x in self.files]
+            self.logger.info('Evaluator() -> self.files={}'.format(self.files))
+
 
     def grade(self, population):
         ''' Grade a whole population '''
-        grading = [(self.evaluate(x), x) for x in population]
+        self.logger.info('Start grade() <- population={}'.format(population))
+        with ThreadPool(len(population)) as p:
+            grading = p.map(self.evaluate, population)
+        # grading = [self.evaluate(x) for x in population]
         grading = sorted(grading, key=lambda score: score[0])
+        self.logger.info('Done grade() -> grading={}'.format(grading))
         return grading
 
     def evaluate(self, key_profile):
         ''' Evaluate a key profile '''
+        self.logger.info('Start evaluate() <- key_profile={}'.format(key_profile))
         kp_string = ''
         for p in key_profile:
             kp_string = kp_string + '{:.12f} '.format(p)
         kp_string = kp_string[:-1]
         total_error = 0
         for filename in self.files:
-            print(filename)
             justkeydding = subprocess.Popen(
                 ('bin/justkeydding',
                 '-e',
@@ -33,7 +42,10 @@ class Evaluator:
             try:
                 score = float(output)
             except ValueError:
+                self.logger.error('Failed while running justkeydding, output: {}'.format(output))
                 score = 0.0
             error = 1 - score
+            self.logger.debug('{}: {}'.format(filename, error))
             total_error += error
-        return total_error
+        self.logger.info('Done evaluate() -> total_error={}'.format(total_error))
+        return (total_error, key_profile)
