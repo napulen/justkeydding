d6c5cb6059e1e93c99a68a492de6ce0fc339b3db
Nestor Napoles <napulen@gmail.com>
2019-03-27 17:00:57 -0400

    New: Adding feature scaling and data augmentation

--- classifier.py
+++ classifier.py
@@ -1,12 +1,36 @@
-import numpy as numpy
+import numpy as np
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import cross_val_score
-
 from sklearn.svm import LinearSVC
 
-X = np.load('dataset_features.pkl')
-y = np.load('dataset_labels.pkl')
+def transpose(Xi, y):
+    is_minor = y // 12
+    trans_features = []
+    trans_labels = [] 
+    for ti in range(12):
+        Xip = Xi.reshape(-1, 12)
+        label = ((y + ti) % 12) + (12 * is_minor)
+        features = np.roll(Xip, ti, axis=1)
+        trans_features.append(features)
+        trans_labels.append(label)
+    return trans_features, trans_labels
 
-random_search = RandomizedSearchCV(model, param_distributions=params, cv=5, verbose=10, random_state=seed, n_iter=10)
+def data_augmentation(X, y):
+    original_length = X.shape[1]
+    for i in range(len(X)):
+        Xp, yp = transpose(X[i], y[i])
+
+def feature_scaling(X):
+    scaler = MinMaxScaler()
+    original_length = X.shape[1]
+    Xp = X.reshape(-1, 24)
+    Xp = np.apply_along_axis(lambda x: scaler.fit_transform(x.reshape(-1, 1)), 1, Xp)
+    Xp = Xp.reshape(-1, original_length)
+    return Xp
+
+if __name__ == '__main__':
+    X = np.load('dataset_features.pkl')
+    y = np.load('dataset_labels.pkl')
+    X = feature_scaling(X)
