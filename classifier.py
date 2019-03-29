6bf38dae1424b13602298e7ffabd5c5c1d4cc6e7
Nestor Napoles <napulen@gmail.com>
2019-03-29 14:04:29 -0400

    New: Adding data-augmentation

--- classifier.py
+++ classifier.py
@@ -8,19 +8,28 @@ from sklearn.svm import LinearSVC
 def transpose(Xi, y):
     is_minor = y // 12
     trans_features = []
-    trans_labels = [] 
+    trans_labels = []
     for ti in range(12):
         Xip = Xi.reshape(-1, 12)
         label = ((y + ti) % 12) + (12 * is_minor)
         features = np.roll(Xip, ti, axis=1)
         trans_features.append(features)
         trans_labels.append(label)
+    trans_features = np.array(trans_features).reshape(12, -1)
+    trans_labels = np.array(trans_labels)
     return np.array(trans_features), np.array(trans_labels)
 
 def data_augmentation(X, y):
     original_length = X.shape[1]
+    trans_features = []
+    trans_labels = []
     for i in range(len(X)):
         Xp, yp = transpose(X[i], y[i])
+        trans_features.append(Xp)
+        trans_labels.append(yp)
+    trans_features = np.array(trans_features).reshape(-1, original_length)
+    trans_labels = np.array(trans_labels).reshape(-1)
+    return trans_features, trans_labels
 
 def feature_scaling(X):
     scaler = MinMaxScaler()
@@ -34,3 +43,7 @@ if __name__ == '__main__':
     X = np.load('dataset_features.pkl')
     y = np.load('dataset_labels.pkl')
     X = feature_scaling(X)
+    Xa, ya = data_augmentation(X, y)
+    Xa = Xa.reshape(-1, 27*24)
+    ya = ya.reshape(-1)
+    clf = LinearSVC(max_iter=10000)
