2e6f1f3108769efd47b58f033da529f79b407fe8
Nestor Napoles Lopez <napulen@gmail.com>
2019-07-23 20:31:52 -0400

    New: Final version of the meta-classifier tuning

--- napoles19key_metaclassifiertuning.py
+++ napoles19key_metaclassifiertuning.py
@@ -19,29 +19,28 @@ if __name__ == '__main__':
     # Feature scaling in all the feature vectors
     experiment1_symbolic.X = dataset.feature_scaling(experiment1_symbolic.X)
 
+    # Data augmentation
+    # experiment1_symbolic.X, experiment1_symbolic.y = dataset.data_augmentation(
+    #         experiment1_symbolic.X,
+    #         experiment1_symbolic.y)
+
     random_seed = 1
 
-    penalty = ['l2']
-    dual = [False]
-    fit_intercept = [True, False]
-    intercept_scaling = np.abs(np.random.normal(1, 0.2, 5).astype(float))
-    solver = ['saga']
-    multi_class = ['auto']
+    hidden_layer_sizes = [(24, 48), (48, 12), (24, 24)]
+    activation = ['tanh', 'relu', 'logistic']
+    alpha = np.abs(np.random.normal(0.0001, 0.001, 20).astype(float))
+    solver = ['lbfgs', 'sgd', 'adam']
     max_iter = [100000]
-    C = np.abs(np.random.normal(1, 0.2, 20).astype(float))
 
-    parameters = { 
-        'C': C,
-        'dual': dual,
-        'fit_intercept': fit_intercept,
-        'intercept_scaling': intercept_scaling,
+    parameters = {
+        'hidden_layer_sizes': hidden_layer_sizes,
+        'activation': activation,
+        'alpha': alpha,
         'solver': solver,
-        'multi_class': multi_class,
-        'penalty': penalty,
         'max_iter': max_iter
     }
 
-    clf = RandomizedSearchCV(LogisticRegression(), param_distributions=parameters, n_iter=50, iid=False, cv=2, random_state=random_seed, verbose=2)
+    clf = RandomizedSearchCV(MLPClassifier(), param_distributions=parameters, n_iter=10, iid=False, cv=2, random_state=random_seed, verbose=10)
 
     clf.fit(experiment1_symbolic.X, experiment1_symbolic.y)
 
