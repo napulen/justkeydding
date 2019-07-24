e21cd34864cc29480c42b41b577376db4cdbb2c7
Nestor Napoles Lopez <napulen@gmail.com>
2019-07-23 20:31:34 -0400

    New: Improving legends of plotting script

--- napoles19key_plotkeyprofiles.py
+++ napoles19key_plotkeyprofiles.py
@@ -4,19 +4,25 @@ import optimizer.key_profiles as kp
 import pandas as pd
 
 if __name__ == '__main__':
-    six_profiles = [
-        'aarden_essen', 'albrecht_shanahan2', 
-        'bellman_budge', 'krumhansl_kessler', 
-        'sapp', 'temperley'
-    ]
-    kps = { k: v for k, v in kp._kp.items() if k in six_profiles }
+    six_profiles = {
+        'aarden_essen': 'Aarden-Essen',
+        'albrecht_shanahan2': 'Albrecht-Shanahan',
+        'bellman_budge': 'Bellman-Budge',
+
+        'krumhansl_kessler': 'Krumhansl-Kessler',
+        'sapp': 'Sapp',
+        'temperley': 'Temperley'
+    }
+    kps = { six_profiles[k]: v for k, v in kp._kp.items() if k in six_profiles }
     df = pd.DataFrame(kps)
-    df['pitchclass'] = list(range(12)) * 2
-    df['mode'] = ['major'] * 12 + ['minor'] * 12
-    df = df.melt(id_vars=['pitchclass', 'mode'], var_name='keyprofile', value_name='probability')
-    df_major = df.loc[df['mode'] == 'major']
-    df_minor = df.loc[df['mode'] == 'minor']
-    
-    ax = sns.catplot(x='pitchclass', y='probability', height=4.0, aspect=2.0, legend_out=False, hue='mode', data=df, kind='swarm')
+    df['Pitch-Class'] = list(range(12)) * 2
+    df['Mode'] = ['Major'] * 12 + ['Minor'] * 12
+    df = df.melt(id_vars=['Pitch-Class', 'Mode'], var_name='Key Profile', value_name='Probability')
+    df_major = df.loc[df['Mode'] == 'Major']
+    df_minor = df.loc[df['Mode'] == 'Minor']
+
+    sns.catplot(x='Pitch-Class', y='Probability', hue='Key Profile', palette='colorblind', row='Mode', height=2, aspect=2.3, legend_out=True, data=df, kind='swarm')
+    # sns.swarmplot(x='Pitch-Class', y='Probability', hue='Key Profile', palette='colorblind', data=df)
+    # sns.swarmplot(x='Pitch-Class', y='Probability', hue='Key Profile', palette='colorblind', data=df_minor, marker=('X'))
     plt.show()
 
