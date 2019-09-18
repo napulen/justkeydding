23c2afc1d7ad7bfa616ee4dcd0c7f9fd435952a3
Nestor Napoles <napulen@gmail.com>
2019-09-18 15:55:52 -0400

    Hardcoding the ensemble info for the release code

--- justkeydding_ensemble.py
+++ justkeydding_ensemble.py
@@ -25,12 +25,24 @@ if __name__ == '__main__':
     if not os.path.exists(model):
         print('You need to train a model first')
         exit()
-    # TODO: Get the key profiles and transitions from the model
-    # not the datasets
-    ensemble = dataset.Dataset('napoles2018_symbolic').ensemble    
-    ###########################################################
-    ens = ensembler.Ensembler(ensemble['key_profiles'], ensemble['key_transitions'])
-    features = ens.evaluate(filename, ensemble['mixed_profiles'])
+    key_profiles = [
+        'aarden_essen',
+        'temperley',
+        'krumhansl_kessler',
+        'bellman_budge',
+        'albrecht_shanahan1',
+        'albrecht_shanahan2',
+        'sapp',
+        'simple_natural_minor',
+        'simple_harmonic_minor',
+        'simple_melodic_minor',]
+    key_transitions = [
+        'ktg_exponential5',
+        'ktg_exponential10',
+        'ktg_exponential15',]
+    mixed_profiles = False
+    ens = ensembler.Ensembler(key_profiles, key_transitions)
+    features = ens.evaluate(filename, mixed_profiles)
     features = [f for l in features for f in l]
     feature_array = np.array(features).reshape(1, -1)
     feature_array = dataset.feature_scaling(feature_array)
