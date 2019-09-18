0003daa46a3f626d6317eab31b1bc3fec136030a
Nestor Napoles <napulen@gmail.com>
2019-09-18 10:39:04 -0400

    Adding feature scaling to justkeydding_ensemble

--- justkeydding_ensemble.py
+++ justkeydding_ensemble.py
@@ -11,14 +11,11 @@ tonic_names = [
     'Ab', 'A', 'Bb', 'B', 
 ]
 
-
 def key_name(key_number):
     mode = 'major' if key_number < 12 else 'minor'
     tonic = tonic_names[key_number % 12]
     return '{}\t{}'.format(tonic, mode)
     
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('You need to provide an input file')
@@ -30,13 +27,13 @@ if __name__ == '__main__':
         exit()
     # TODO: Get the key profiles and transitions from the model
     # not the datasets
-    ensemble = dataset.Dataset('napoles2018_symbolic').ensemble
-    # print(ensemble)
+    ensemble = dataset.Dataset('napoles2018_symbolic').ensemble    
     ###########################################################
     ens = ensembler.Ensembler(ensemble['key_profiles'], ensemble['key_transitions'])
     features = ens.evaluate(filename, ensemble['mixed_profiles'])
     features = [f for l in features for f in l]
     feature_array = np.array(features).reshape(1, -1)
+    feature_array = dataset.feature_scaling(feature_array)
     clf = joblib.load(model)
     prediction = clf.predict(feature_array)[0]
     print(key_name(prediction))
